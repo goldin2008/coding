b94d48453cc734e7f6307a6a95e25d2a72825e92
goldin2008 <goldin20082011@gmail.com>
2024-02-05 23:33:24 -0500

    Update

--- sorting.py
+++ sorting.py
@@ -1,3 +1,4 @@
+# Approach 1: Merge Sort
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         temp_arr = [0] * len(nums)
@@ -51,4 +52,110 @@ class Solution:
         merge_sort(0, len(nums) - 1)
         return nums
     
+# Approach 2: Heap Sort
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        # Function to heapify a subtree (in top-down order) rooted at index i.
+        def heapify(n: int, i: int):
+            # Initialize largest as root 'i'.
+            largest = i;
+            left = 2 * i + 1
+            right = 2 * i + 2
+            # If left child is larger than root.
+            if left < n and nums[left] > nums[largest]:
+                largest = left
+            # If right child is larger than largest so far.
+            if right < n and nums[right] > nums[largest]:
+                largest = right
+            # If largest is not root swap root with largest element
+            # Recursively heapify the affected sub-tree (i.e. move down).
+            if largest != i:
+                nums[i], nums[largest] =  nums[largest], nums[i]
+                heapify(n, largest)
+
+        def heap_sort():
+            n = len(nums)
+            # Build heap; heapify (top-down) all elements except leaf nodes.
+            for i in range(n // 2 - 1, -1, -1):
+                heapify(n, i)
+            # Traverse elements one by one, to move current root to end, and
+            for i in range(n - 1, -1, -1):
+                nums[0], nums[i] = nums[i], nums[0]
+                # call max heapify on the reduced heap.
+                heapify(i, 0)
+
+        heap_sort()
+        return nums
+    
+    # Approach 3: Counting Sort
+    class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def counting_sort():
+            # Create the counting hash map.
+            counts = {}
+            # Find the minimum and maximum values in the array.
+            minVal, maxVal = min(nums), max(nums)
+            # Update element's count in the hash map.
+            for val in nums:
+                counts[val] = counts.get(val, 0) + 1
+
+            index = 0
+            # Place each element in its correct position in the array.
+            for val in range(minVal, maxVal + 1, 1):
+                # Append all 'val's together if they exist.
+                while counts.get(val, 0) > 0:
+                    nums[index] = val
+                    index += 1
+                    counts[val] -= 1
+
+        counting_sort()
+        return nums
+    
+    # Approach 4: Radix Sort
+    class Solution:
+    # Radix sort function.
+    def radix_sort(self, nums: List[int]) -> List[int]:
+        # Find the absolute maximum element to find max number of digits.
+        max_element = nums[0]
+        for val in nums:
+            max_element = max(abs(val), max_element)
+
+        max_digits = 0
+        while max_element > 0:
+            max_digits += 1
+            max_element = max_element // 10
+
+        place_value = 1
+        
+        # Bucket sort function for each place value digit.
+        def bucket_sort():
+            buckets = [[] for i in range(10)]
+            # Store the respective number based on it's digit.
+            for val in nums:
+                digit = abs(val) / place_value
+                digit = int(digit % 10)
+                buckets[digit].append(val)
+
+            # Overwrite 'nums' in sorted order of current place digits.
+            index = 0
+            for digit in range(10):
+                for val in buckets[digit]:
+                    nums[index] = val
+                    index += 1
+
+        # Radix sort, least significant digit place to most significant.      
+        for _ in range(max_digits):
+            bucket_sort()
+            place_value *= 10
+
+        # Seperate out negatives and reverse them. 
+        positives = [val for val in nums if val >= 0]
+        negatives = [val for val in nums if val < 0]
+        negatives.reverse()
+
+        # Final 'arr' will be 'negative' elements, then 'positive' elements.
+        return negatives + positives
+            
+    def sortArray(self, nums: List[int]) -> List[int]:  
+        return self.radix_sort(nums)                                                      
     
