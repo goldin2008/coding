e4257a01925b57f5106c7cdfe195594726da4eca
goldin2008 <goldin20082011@gmail.com>
2024-02-22 17:07:43 -0500

    Update

--- dmsxl/basic_concept.py
+++ dmsxl/basic_concept.py
@@ -1,3 +1,6 @@
 Basics of Bit Manipulation
 
 https://www.hackerearth.com/practice/basic-programming/bit-manipulation/basics-of-bit-manipulation/tutorial/
+
+by applying (id+1)^1-1, you toggle the LSB of the input number, which changes odd numbers to even and even numbers to odd. 
+This operation works because it takes advantage of the properties of bitwise XOR and subtraction to toggle and reset the LSB, respectively.
