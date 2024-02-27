694f10d991a8e17ac2fa57f92d8a6a8aaf70c2a1
goldin2008 <goldin20082011@gmail.com>
2024-02-27 13:38:48 -0500

    Update

--- dmsxl/sql.py
+++ dmsxl/sql.py
@@ -1,1 +1,5 @@
 # (product_id, change_date) is the primary key (combination of columns with unique values) of this table.
+
+# In MySQL, they say the window function performs an aggregate-like operation on a set of query rows. 
+# Even though they work almost the same, the aggregate function returns a single row for each target field, 
+# but the window function produces a result for each row.
