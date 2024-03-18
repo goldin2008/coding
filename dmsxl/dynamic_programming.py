76b941f317150c99e9b7379ddce28b8680e78d55
goldin2008 <goldin20082011@gmail.com>
2024-03-17 23:32:22 -0400

    Update

--- dmsxl/dynamic_programming.py
+++ dmsxl/dynamic_programming.py
@@ -1788,7 +1788,6 @@ class Solution:
 
 #27 309.最佳买卖股票时机含冷冻期
 # 版本一
-from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
@@ -1803,28 +1802,28 @@ class Solution:
             dp[i][3] = dp[i-1][2]  # 当前不持有股票且当天卖出后处于冷冻期的最大利润等于前一天不持有股票且不处于冷冻期的最大利润
         return max(dp[n-1][3], dp[n-1][1], dp[n-1][2])  # 返回最后一天不持有股票的最大利润
 # 版本二
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices)
-        if n < 2:
-            return 0
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         n = len(prices)
+#         if n < 2:
+#             return 0
 
-        # 定义三种状态的动态规划数组
-        dp = [[0] * 3 for _ in range(n)]
-        dp[0][0] = -prices[0]  # 持有股票的最大利润
-        dp[0][1] = 0           # 不持有股票,且处于冷冻期的最大利润
-        dp[0][2] = 0           # 不持有股票,不处于冷冻期的最大利润
+#         # 定义三种状态的动态规划数组
+#         dp = [[0] * 3 for _ in range(n)]
+#         dp[0][0] = -prices[0]  # 持有股票的最大利润
+#         dp[0][1] = 0           # 不持有股票,且处于冷冻期的最大利润
+#         dp[0][2] = 0           # 不持有股票,不处于冷冻期的最大利润
 
-        for i in range(1, n):
-            # 当前持有股票的最大利润等于前一天持有股票的最大利润或者前一天不持有股票且不处于冷冻期的最大利润减去当前股票的价格
-            dp[i][0] = max(dp[i-1][0], dp[i-1][2] - prices[i])
-            # 当前不持有股票且处于冷冻期的最大利润等于前一天持有股票的最大利润加上当前股票的价格
-            dp[i][1] = dp[i-1][0] + prices[i]
-            # 当前不持有股票且不处于冷冻期的最大利润等于前一天不持有股票的最大利润或者前一天处于冷冻期的最大利润
-            dp[i][2] = max(dp[i-1][2], dp[i-1][1])
+#         for i in range(1, n):
+#             # 当前持有股票的最大利润等于前一天持有股票的最大利润或者前一天不持有股票且不处于冷冻期的最大利润减去当前股票的价格
+#             dp[i][0] = max(dp[i-1][0], dp[i-1][2] - prices[i])
+#             # 当前不持有股票且处于冷冻期的最大利润等于前一天持有股票的最大利润加上当前股票的价格
+#             dp[i][1] = dp[i-1][0] + prices[i]
+#             # 当前不持有股票且不处于冷冻期的最大利润等于前一天不持有股票的最大利润或者前一天处于冷冻期的最大利润
+#             dp[i][2] = max(dp[i-1][2], dp[i-1][1])
 
-        # 返回最后一天不持有股票的最大利润
-        return max(dp[-1][1], dp[-1][2])
+#         # 返回最后一天不持有股票的最大利润
+#         return max(dp[-1][1], dp[-1][2])
 
 
 #28 714.买卖股票的最佳时机含手续费
