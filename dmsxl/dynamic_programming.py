662718c223ae78afc3ca52dfb0db372576109af9
goldin2008 <goldin20082011@gmail.com>
2024-03-17 23:48:12 -0400

    Update

--- dmsxl/dynamic_programming.py
+++ dmsxl/dynamic_programming.py
@@ -1677,17 +1677,17 @@ class Solution:
             dp[i][0] = max(dp[i-1][0], -prices[i])
             dp[i][1] = max(dp[i-1][1], prices[i] + dp[i-1][0])
         return dp[-1][1]
-# # 动态规划:版本二
-# class Solution:
-#     def maxProfit(self, prices: List[int]) -> int:
-#         length = len(prices)
-#         dp = [[0] * 2 for _ in range(2)] #注意这里只开辟了一个2 * 2大小的二维数组
-#         dp[0][0] = -prices[0]
-#         dp[0][1] = 0
-#         for i in range(1, length):
-#             dp[i % 2][0] = max(dp[(i-1) % 2][0], -prices[i])
-#             dp[i % 2][1] = max(dp[(i-1) % 2][1], prices[i] + dp[(i-1) % 2][0])
-#         return dp[(length-1) % 2][1]
+# *** 动态规划:版本二
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        length = len(prices)
+        dp = [[0] * 2 for _ in range(2)] #注意这里只开辟了一个2 * 2大小的二维数组
+        dp[0][0] = -prices[0]
+        dp[0][1] = 0
+        for i in range(1, length):
+            dp[i % 2][0] = max(dp[(i-1) % 2][0], -prices[i])
+            dp[i % 2][1] = max(dp[(i-1) % 2][1], prices[i] + dp[(i-1) % 2][0])
+        return dp[(length-1) % 2][1]
 # *** 动态规划:版本三
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
@@ -1711,17 +1711,17 @@ class Solution:
             dp[i][0] = max(dp[i-1][0], dp[i-1][1] - prices[i]) #注意这里是和121. 买卖股票的最佳时机唯一不同的地方
             dp[i][1] = max(dp[i-1][1], dp[i-1][0] + prices[i])
         return dp[-1][1]
-# 版本二
-# class Solution:
-#     def maxProfit(self, prices: List[int]) -> int:
-#         length = len(prices)
-#         dp = [[0] * 2 for _ in range(2)] #注意这里只开辟了一个2 * 2大小的二维数组
-#         dp[0][0] = -prices[0]
-#         dp[0][1] = 0
-#         for i in range(1, length):
-#             dp[i % 2][0] = max(dp[(i-1) % 2][0], dp[(i-1) % 2][1] - prices[i])
-#             dp[i % 2][1] = max(dp[(i-1) % 2][1], dp[(i-1) % 2][0] + prices[i])
-#         return dp[(length-1) % 2][1]
+# *** 版本二
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        length = len(prices)
+        dp = [[0] * 2 for _ in range(2)] #注意这里只开辟了一个2 * 2大小的二维数组
+        dp[0][0] = -prices[0]
+        dp[0][1] = 0
+        for i in range(1, length):
+            dp[i % 2][0] = max(dp[(i-1) % 2][0], dp[(i-1) % 2][1] - prices[i])
+            dp[i % 2][1] = max(dp[(i-1) % 2][1], dp[(i-1) % 2][0] + prices[i])
+        return dp[(length-1) % 2][1]
 
 
 #25 123.买卖股票的最佳时机III
@@ -1741,19 +1741,19 @@ class Solution:
             dp[i][4] = max(dp[i-1][4], dp[i-1][3] + prices[i])
         return dp[-1][4]
 # 版本二
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) == 0:
-            return 0
-        dp = [0] * 5 
-        dp[1] = -prices[0]
-        dp[3] = -prices[0]
-        for i in range(1, len(prices)):
-            dp[1] = max(dp[1], dp[0] - prices[i])
-            dp[2] = max(dp[2], dp[1] + prices[i])
-            dp[3] = max(dp[3], dp[2] - prices[i])
-            dp[4] = max(dp[4], dp[3] + prices[i])
-        return dp[4]
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         if len(prices) == 0:
+#             return 0
+#         dp = [0] * 5 
+#         dp[1] = -prices[0]
+#         dp[3] = -prices[0]
+#         for i in range(1, len(prices)):
+#             dp[1] = max(dp[1], dp[0] - prices[i])
+#             dp[2] = max(dp[2], dp[1] + prices[i])
+#             dp[3] = max(dp[3], dp[2] - prices[i])
+#             dp[4] = max(dp[4], dp[3] + prices[i])
+#         return dp[4]
 
 
 #26 188.买卖股票的最佳时机IV
