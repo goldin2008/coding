a2d614651526cd064ca0f1965032f7726fda9d20
goldin2008 <goldin20082011@gmail.com>
2024-03-20 16:14:23 -0400

    Update

--- dmsxl/dynamic_programming.py
+++ dmsxl/dynamic_programming.py
@@ -2115,6 +2115,7 @@ class Solution:
 
 
 #36 115.不同的子序列
+    # 这道题目如果不是子序列，而是要求连续序列的，那就可以考虑用KMP。
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         dp = [[0] * (len(t)+1) for _ in range(len(s)+1)]
@@ -2125,8 +2126,14 @@ class Solution:
         for i in range(1, len(s)+1):
             for j in range(1, len(t)+1):
                 if s[i-1] == t[j-1]:
+                    # 当s[i - 1] 与 t[j - 1]相等时，dp[i][j]可以有两部分组成。
+                    # 一部分是用s[i - 1]来匹配，那么个数为dp[i - 1][j - 1]。
+                    # 即不需要考虑当前s子串和t子串的最后一位字母，所以只需要 dp[i-1][j-1]。
+                    # 一部分是不用s[i - 1]来匹配，个数为dp[i - 1][j]。
                     dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
                 else:
+                    # 当s[i - 1] 与 t[j - 1]不相等时，dp[i][j]只有一部分组成，
+                    # 不用s[i - 1]来匹配（就是模拟在s中删除这个元素），即：dp[i - 1][j]
                     dp[i][j] = dp[i-1][j]
         return dp[-1][-1]
 
