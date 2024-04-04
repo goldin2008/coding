6645b3a0e85c61a6cc77202e66d78146cf380be8
goldin2008 <goldin20082011@gmail.com>
2024-04-04 17:36:55 -0400

    Update

--- dmsxl/08_backtracking.py
+++ dmsxl/08_backtracking.py
@@ -1369,7 +1369,7 @@ class Solution:
 #                 used[i] = False
 
 
-#13 332.重新安排行程
+#13 ??? 332.重新安排行程
     # 给定一个机票的字符串二维数组 [from, to], 子数组中的两个成员分别表示飞机出发和降落的机场地点, 对该行程进行重新规划排序。所有这些机票都属于一个从 JFK（肯尼迪国际机场）出发的先生, 所以该行程必须从 JFK 开始。
     # 输入：[["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
     # 输出：["JFK", "MUC", "LHR", "SFO", "SJC"]
@@ -1498,7 +1498,7 @@ class Solution:
         if not n: return []
         board = [['.'] * n for _ in range(n)]
         res = []
-        def isVaild(board,row, col):
+        def isVaild(board, row, col):
             #判断同一列是否冲突
             for i in range(len(board)):
                 if board[i][col] == 'Q':
