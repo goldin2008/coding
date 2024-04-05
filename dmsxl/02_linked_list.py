eaaf4585216ad44cfa2ab01b1795e4928f1cf545
goldin2008 <goldin20082011@gmail.com>
2024-04-04 23:58:23 -0400

    Update

--- dmsxl/02_linked_list.py
+++ dmsxl/02_linked_list.py
@@ -307,6 +307,7 @@ class Solution:
     # 输入：head = [1,2,3,4,5], n = 2 输出：[1,2,3,5] 示例 2：
     # 输入：head = [1], n = 1 输出：[] 示例 3：
     # 输入：head = [1,2], n = 1 输出：[1]
+# 双指针
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
