4b5a50ab05b7a66420fa97756acf051e7dbce1e9
goldin2008 <goldin20082011@gmail.com>
2024-07-24 23:35:35 -0400

    Update

--- dmsxl/13_extra.py
+++ dmsxl/13_extra.py
@@ -1151,6 +1151,7 @@ class Solution:
     # 示例 4：
     # 输入：s = "ac"
     # 输出："a"
+# 布尔类型的dp[i][j]：表示区间范围[i,j] （注意是左闭右闭）的子串是否是回文子串，如果是dp[i][j]为true，否则为false
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         dp = [[False] * len(s) for _ in range(len(s))]
@@ -1211,6 +1212,38 @@ class Solution:
                 self.maxLength = j - i + 1
             i -= 1
             j += 1
+# 647
+# 双指针法：
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        result = 0
+        for i in range(len(s)):
+            result += self.extend(s, i, i, len(s)) #以i为中心
+            result += self.extend(s, i, i+1, len(s)) #以i和i+1为中心
+        return result
+    
+    def extend(self, s, i, j, n):
+        res = 0
+        while i >= 0 and j < n and s[i] == s[j]:
+            i -= 1
+            j += 1
+            res += 1
+        return res
+# 动规
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        dp = [[False] * len(s) for _ in range(len(s))]
+        result = 0
+        for i in range(len(s)-1, -1, -1): #注意遍历顺序
+            for j in range(i, len(s)):
+                if s[i] == s[j]:
+                    if j - i <= 1: #情况一 和 情况二
+                        dp[i][j] = True
+                        result += 1
+                    elif dp[i+1][j-1]: #情况三
+                        dp[i][j] = True
+                        result += 1
+        return result
 
 
 #26 132. 分割回文串 II
