7a23a4551b7f686517672e554c43fcde1ecd981d
goldin2008 <goldin20082011@gmail.com>
2025-02-14 15:07:16 -0500

    Update

--- dmsxl/Xothers.py
+++ dmsxl/Xothers.py
@@ -38,3 +38,28 @@ def hamming_weights_of_integers(n: int) -> List[int]:
         dp[x] = dp[x >> 1] + (x & 1)
     return dp
 
+# Lonely Integer
+    # Given an integer array where each number occurs twice except for one of them, find the unique number.
+    # Example:
+    # Input: nums = [1, 3, 3, 2, 1]
+    # Output: 2
+from typing import List
+
+def lonely_integer(nums: List[int]) -> int:
+    # Write your code here
+    res = 0
+    for num in nums:
+        res ^= num
+    return res
+
+# Swap Odd and Even Bits
+    # Given an unsigned 32-bit integer n, return an integer where all of 
+    # n's even bits are swapped with their adjacent odd bits.
+def swap_odd_and_even_bits(n: int) -> int:
+    # Write your code here
+    even_mask = 0x55555555
+    odd_mask = 0xAAAAAAAA
+    even_bits = n & even_mask
+    odd_bits = n & odd_mask
+    return (even_bits << 1) | (odd_bits >> 1)
+
