72866e34dafe70f50e8b0ada288157e52504f7a7
goldin2008 <goldin20082011@gmail.com>
2025-02-23 23:47:53 -0500

    Update

--- dmsxl/sql.py
+++ dmsxl/sql.py
@@ -23,6 +23,44 @@
 # The dollar sign: This represents the end of a string or line.
 
 # 1341. Movie Rating
+# Table: Movies
+# +---------------+---------+
+# | Column Name   | Type    |
+# +---------------+---------+
+# | movie_id      | int     |
+# | title         | varchar |
+# +---------------+---------+
+# movie_id is the primary key (column with unique values) for this table.
+# title is the name of the movie.
+ 
+# Table: Users
+# +---------------+---------+
+# | Column Name   | Type    |
+# +---------------+---------+
+# | user_id       | int     |
+# | name          | varchar |
+# +---------------+---------+
+# user_id is the primary key (column with unique values) for this table.
+# The column 'name' has unique values.
+
+# Table: MovieRating
+# +---------------+---------+
+# | Column Name   | Type    |
+# +---------------+---------+
+# | movie_id      | int     |
+# | user_id       | int     |
+# | rating        | int     |
+# | created_at    | date    |
+# +---------------+---------+
+# (movie_id, user_id) is the primary key (column with unique values) for this table.
+# This table contains the rating of a movie by a user in their review.
+# created_at is the user's review date. 
+ 
+# Write a solution to:
+
+# Find the name of the user who has rated the greatest number of movies. In case of a tie, return the lexicographically smaller user name.
+# Find the movie name with the highest average rating in February 2020. In case of a tie, return the lexicographically smaller movie name.
+# The result format is in the following example.
 (SELECT name AS results
 FROM MovieRating JOIN Users USING(user_id)
 GROUP BY name
